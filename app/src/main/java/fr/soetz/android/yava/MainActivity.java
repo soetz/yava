ad7d33433268b6d71d54f528495822e6a305b68f
Unknown <vernythiago@gmail.com>
2018-03-23 17:26:10 +0100

    List

--- app/src/main/java/fr/soetz/android/yava/MainActivity.java
+++ app/src/main/java/fr/soetz/android/yava/MainActivity.java
@@ -2,12 +2,36 @@ package fr.soetz.android.yava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView liste;
+    private Button loadButton;
+    private String[] objets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        liste = (ListView)findViewById(R.id.liste);
+        loadButton = (Button) findViewById(R.id.loadbtn);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, objets);
+        liste.setAdapter(adapter);
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new VelovAsyncTask().execute("url");
+            }
+        });
+
     }
 }
