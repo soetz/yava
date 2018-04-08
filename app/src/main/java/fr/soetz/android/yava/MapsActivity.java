9a81bb68acd8ecac619b17d1a1911e5d6eb90a67
Simon Lecutiez <simon.lecutiez@gmail.com>
2018-04-08 04:12:07 +0200

    Ajout du marqueur pour les stations bonus, de la localisation de l'utilisateur, de l'image de launcher, du splashscreen ainsi que de la gestion des permissions

--- app/src/main/java/fr/soetz/android/yava/MapsActivity.java
+++ app/src/main/java/fr/soetz/android/yava/MapsActivity.java
@@ -1,5 +1,6 @@
 package fr.soetz.android.yava;
 
+import android.Manifest;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -7,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -26,24 +28,84 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.lypeer.fcpermission.FcPermissions;
+import com.lypeer.fcpermission.impl.FcPermissionsCallbacks;
 
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, FcPermissionsCallbacks {
 
     private static GoogleMap mMap;
+    private static boolean isReady = false;
     private static List<Station> STATIONS_LIST;
     private static List<Marker> MARKERS_LIST = new ArrayList<Marker>();
     private static String mode = "pickup";
     LocationManager locationManager;
+    private static Location CURRENT_LOCATION;
+    private static Marker CURRENT_LOCATION_MARKER;
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(CURRENT_LOCATION_MARKER != null){
+                CURRENT_LOCATION_MARKER.remove();
+            }
+
+            CURRENT_LOCATION = location;
+
+            if(isReady){
+                Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+                Bitmap bmp = Bitmap.createBitmap(80, 80, conf);
+                Canvas canvas1 = new Canvas(bmp);
+
+                // paint defines the text color, stroke width and size
+                Paint textColor = new Paint();
+                textColor.setTextSize(40);
+                textColor.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+                textColor.setColor(Color.WHITE);
+
+                Paint borderColor = new Paint();
+                borderColor.setColor(Color.rgb(245, 245, 245));
+                Paint backgroundColor = new Paint();
+                backgroundColor.setColor(Color.rgb(66, 134, 244));
+
+                canvas1.drawCircle(30, 30, 30, borderColor);
+                canvas1.drawCircle(30, 30, 20, backgroundColor);
+
+                LatLng latLng = new LatLng(CURRENT_LOCATION.getLatitude(), CURRENT_LOCATION.getLongitude());
+                CURRENT_LOCATION_MARKER = mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .icon(BitmapDescriptorFactory.fromBitmap(bmp)));
+            }
+
+            Log.d("pos", location.toString());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+
+        FcPermissions.requestPermissions(this, "L'expérience est meilleure si vous nous autorisez à accéder à votre position !\nVous devrez redémarrer l'application pour que ce choix prenne effet.", FcPermissions.REQ_PER_CODE, Manifest.permission.ACCESS_FINE_LOCATION);
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -67,6 +129,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 changeMode(isChecked);
             }
         });
+
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if(isGPSEnabled) {
+                CURRENT_LOCATION = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5.0f, mLocationListener);
+            }
+        }
+        catch (SecurityException e){
+
+        }
     }
 
 
@@ -82,6 +156,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        isReady = true;
 
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
             @Override
@@ -91,62 +166,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public View getInfoContents(Marker marker) {
-                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View v = inflater.inflate(R.layout.info_window, null);
+                if(marker.getTitle() != null){
 
-                TextView title = (TextView) v.findViewById(R.id.title);
-                title.setText(marker.getTitle());
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    View v = inflater.inflate(R.layout.info_window, null);
 
-                try {
-                    JSONObject data = new JSONObject(marker.getSnippet());
+                    TextView title = (TextView) v.findViewById(R.id.title);
+                    title.setText(marker.getTitle());
 
-                    TextView status = (TextView) v.findViewById(R.id.status);
-                    status.setText("Statut : " + data.getString("status"));
+                    try {
+                        JSONObject data = new JSONObject(marker.getSnippet());
 
-                    TextView address = (TextView) v.findViewById(R.id.address);
-                    address.setText("Adresse : " + data.getString("address"));
+                        TextView status = (TextView) v.findViewById(R.id.status);
+                        status.setText("Statut : " + data.getString("status"));
 
-                    TextView banking = (TextView) v.findViewById(R.id.banking);
-                    banking.setText("Borne de paiement : " + data.getString("banking"));
+                        TextView address = (TextView) v.findViewById(R.id.address);
+                        address.setText("Adresse : " + data.getString("address"));
 
-                    TextView capacity = (TextView) v.findViewById(R.id.capacity);
-                    capacity.setText("Capacité : " + data.getString("capacity"));
+                        TextView banking = (TextView) v.findViewById(R.id.banking);
+                        banking.setText("Borne de paiement : " + data.getString("banking"));
 
-                    TextView bikes = (TextView) v.findViewById(R.id.bikes);
-                    bikes.setText("Vélos : " + data.getString("bikes"));
+                        TextView bonus = (TextView) v.findViewById(R.id.bonus);
+                        bonus.setText("Station bonus : " + data.getString("bonus"));
 
-                    TextView bikestands = (TextView) v.findViewById(R.id.bikestands);
-                    bikestands.setText("Places libres : " + data.getString("available"));
+                        TextView capacity = (TextView) v.findViewById(R.id.capacity);
+                        capacity.setText("Capacité : " + data.getString("capacity"));
+
+                        TextView bikes = (TextView) v.findViewById(R.id.bikes);
+                        bikes.setText("Vélos : " + data.getString("bikes"));
+
+                        TextView bikestands = (TextView) v.findViewById(R.id.bikestands);
+                        bikestands.setText("Places libres : " + data.getString("available"));
+                    }
+                    catch(Exception e){
+                    }
+
+                    return(v);
                 }
-                catch(Exception e){
-                }
-
-                return(v);
+                return(null);
             }
         });
 
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                marker.showInfoWindow();
-                return false;
-            }
-        });
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if(isGPSEnabled){
-            try {
-                Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                LatLng actualPosition = new LatLng(loc.getLatitude(), loc.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(actualPosition));
-                mMap.moveCamera(CameraUpdateFactory.zoomTo(16.0f));
-            }
-            catch(SecurityException e){
-                LatLng lyon = new LatLng(45.75, 4.85);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(lyon));
-                mMap.moveCamera(CameraUpdateFactory.zoomTo(12.0f));
-            }
+        if(CURRENT_LOCATION != null) {
+            LatLng actual = new LatLng(CURRENT_LOCATION.getLatitude(), CURRENT_LOCATION.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(actual));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(18.0f));
+        }
+        else {
+            LatLng lyon = new LatLng(45.75, 4.85);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(lyon));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(12.0f));
         }
     }
 
@@ -173,7 +242,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
             Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-            Bitmap bmp = Bitmap.createBitmap(80, 80, conf);
+            Bitmap bmp = Bitmap.createBitmap(100, 100, conf);
             Canvas canvas1 = new Canvas(bmp);
 
             // paint defines the text color, stroke width and size
@@ -182,57 +251,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             textColor.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
             textColor.setColor(Color.WHITE);
 
+            Paint bonusColor = new Paint();
+            bonusColor.setTextSize(70);
+            bonusColor.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+            bonusColor.setColor(Color.WHITE);
+
+            Paint bonusOutline = new Paint();
+            bonusOutline.setTextSize(70);
+            bonusOutline.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+            bonusOutline.setStyle(Paint.Style.STROKE);
+            bonusOutline.setStrokeWidth(4);
+
             Paint backgroundColor = new Paint();
 
             if(station.getStatus().equals("OPEN")){
                 if(count == 0){
                     backgroundColor.setColor(Color.rgb(173, 32, 32));
+                    bonusOutline.setColor(Color.rgb(173, 32, 32));
                 }
-                else if(count == 1){
-                    backgroundColor.setColor(Color.rgb(175, 119, 28));
-                }
-                else if(count == 2){
-                    backgroundColor.setColor(Color.rgb(175, 148, 28));
-                }
-                else if(count == 3){
-                    backgroundColor.setColor(Color.rgb(129, 140, 30));
+                else if(count <= 3){
+                    backgroundColor.setColor(Color.rgb(216, 166, 17));
+                    bonusOutline.setColor(Color.rgb(216, 166, 17));
                 }
                 else {
                     backgroundColor.setColor(Color.rgb(95, 175, 77));
+                    bonusOutline.setColor(Color.rgb(95, 175, 77));
                 }
             }
             else if(station.getStatus().equals("CLOSED")){
-                backgroundColor.setColor(Color.rgb(173, 32, 32));
+                backgroundColor.setColor(Color.BLACK);
             }
             else {
                 backgroundColor.setColor(Color.GRAY);
             }
 
-            canvas1.drawCircle(40, 40, 40, backgroundColor);
+            canvas1.drawCircle(50, 50, 40, backgroundColor);
 
-            int xPos = 40 - (int)(textColor.measureText(new Integer(count).toString())/2);
+            String text;
+
+            if(station.getStatus().equals("OPEN")){
+                text = new Integer(count).toString();
+            }
+            else {
+                text = "F";
+            }
+            int xPos = 50 - (int)(textColor.measureText(text)/2);
             int yPos = (int) ((canvas1.getHeight() / 2) - ((textColor.descent() + textColor.ascent()) / 2)) ;
 
-            canvas1.drawText(new Integer(count).toString(), xPos, yPos, textColor);
+            canvas1.drawText(text, xPos, yPos, textColor);
+
+            String bonusText = "";
+            if(station.isBonus()){
+                bonusText = "+";
+            }
+
+            xPos = 50 - (int)(bonusColor.measureText(bonusText)/2);
+            yPos = (int) ((canvas1.getHeight() / 2) - ((bonusColor.descent() + bonusColor.ascent()) / 2)) ;
+
+            canvas1.drawText(bonusText, xPos + 30, yPos - 30, bonusColor);
+            canvas1.drawText(bonusText, xPos + 30, yPos - 30, bonusOutline);
 
             LatLng coords = new LatLng(station.getPosition().getLatitude(), station.getPosition().getLongitude());
+
             String banking = "Non";
             if(station.isBanking()){
                 banking = "Oui";
             }
-            String statut = "Inconnu";
+
+            String bonus = "Non";
+            if(station.isBonus()){
+                bonus = "Oui";
+            }
+
+            String status = "Inconnu";
             if(station.getStatus().equals("OPEN")){
-                statut = "Ouvert";
+                status = "Ouvert";
             }
             else if(station.getStatus().equals("CLOSED")){
-                statut = "Fermé";
+                status = "Fermé";
             }
 
             JSONObject data = new JSONObject();
             try {
-                data.put("status", statut);
+                data.put("status", status);
                 data.put("address", station.getAddress());
                 data.put("banking", banking);
+                data.put("bonus", bonus);
                 data.put("capacity", station.getBikeStands());
                 data.put("bikes", station.getAvailableBikes());
                 data.put("available", station.getAvailableBikeStands());
@@ -260,5 +364,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         updateMap();
+    }
+
+    @Override
+    public void onPermissionsGranted(int i, List<String> list) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int i, List<String> list) {
     }
 }
